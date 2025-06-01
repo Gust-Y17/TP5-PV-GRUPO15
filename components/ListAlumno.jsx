9f25623a86008ea2264d4ae9ea94fa156715e4ff
gustavo yevara <gg1904105@gmail.com>
2025-06-01 18:30:35 -0300

    nejora en list alumno por gustavo yevara

--- components/ListAlumno.jsx
+++ components/ListAlumno.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import ItemAlumno from "./ItemAlumno"
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const ListAlumno = ({ alumnos, onEditar, onEliminar, onVerDetalles }) => {
 const [alumnoAEditar,setAlumnoAEditar] = useState (null)
@@ -16,12 +18,13 @@ const guardarEdicion = (alumnoAEditar) => {
   
   return (
     <Container>
-      <h3>Lista de Alumnos</h3>
 
       {alumnos.length === 0 ? (
-        <p>No hay alumnos.</p>
+        <p>NO HAY ALUMNOS AGREGADOS POR EL MOMENTO</p>
       ) : (
-        <div>
+
+        <Row>
+            <h3>Lista de Alumnos</h3>
           {alumnos.map((alumno) => (
            
             <div key={alumno.id}>
@@ -32,19 +35,19 @@ const guardarEdicion = (alumnoAEditar) => {
                 onCancelar={()=> setAlumnoAEditar(null)}
                 />
               ) : (
-             <div className="alumno">
-                <p><strong>Nombre:</strong> {alumno.nombre}</p>
-                <div className="boton">
-                  <button onClick={() => handleEditar(alumno)}>Editar</button>
-                  <button onClick={() => onEliminar(alumno.id)}>Eliminar</button>
-                  <button onClick={() => onVerDetalles(alumno.id)}>Ver Detalles</button>
+                <div className="alumno">
+                  <p><strong>Nombre:</strong> {alumno.nombre}</p>
+                  <Col className="boton">
+                    <button onClick={() => handleEditar(alumno)}>Editar</button>
+                    <button onClick={() => onEliminar(alumno.id)}>Eliminar</button>
+                    <button onClick={() => onVerDetalles(alumno.id)}>Ver Detalles</button>
+                  </Col>
                 </div>
+                )}
             </div>
-          )}
-        </div>
-      ))}
-    </div>
-  )};
+        ))}
+        </Row>
+    )}
   </Container>
 );
 };
