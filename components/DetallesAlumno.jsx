6c25d1cc3dbb093c87ade718b9b495c39a1101ab
gustavo yevara <gg1904105@gmail.com>
2025-06-01 21:24:23 -0300

    mejora en detallealumno por gustavo yevara

--- components/DetallesAlumno.jsx
+++ components/DetallesAlumno.jsx
@@ -1,20 +1,28 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import Row from 'react-bootstrap/Row';
+import Col from "react-bootstrap/Col";
 
 const DetallesAlumno = ({ alumno, onCerrar }) => {
     if (!alumno) return null;
 
     return (
-        <div className="detalles-alumno">
-            <h2>Detalles del Alumno</h2>
-            <p><strong>LU:</strong> {alumno.lu}</p>
-            <p><strong>Nombre:</strong> {alumno.nombre}</p>
-            <p><strong>Apellido:</strong> {alumno.apellido}</p>
-            <p><strong>Curso:</strong> {alumno.curso}</p>
-            <p><strong>Email:</strong> {alumno.email}</p>
-            <p><strong>Domicilio:</strong> {alumno.domicilio}</p>
-            <p><strong>Teléfono:</strong> {alumno.numeroTelefono}</p>
-            <button onClick={onCerrar}>Cerrar</button>
-        </div>
+        <Container className="detalles-alumno">
+            <Row>
+                <Col>
+                <h2>Detalles del Alumno</h2>
+                <p><strong>LU:</strong> {alumno.lu}</p>
+                <p><strong>Nombre:</strong> {alumno.nombre}</p>
+                <p><strong>Apellido:</strong> {alumno.apellido}</p>
+                <p><strong>Curso:</strong> {alumno.curso}</p>
+                <p><strong>Email:</strong> {alumno.email}</p>
+                <p><strong>Domicilio:</strong> {alumno.domicilio}</p>
+                <p><strong>Teléfono:</strong> {alumno.numeroTelefono}</p>
+            <Button onClick={onCerrar}>Cerrar</Button>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
